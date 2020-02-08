65d38ba7c873285269079d444600f520f18c8523
GoodGuyGregory <greg.witt625@gmail.com>
2020-02-07 19:34:29 -0500

    inital commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## React Giphy Search Project
+
+**Checkout the Environment to View Project**
+
+
---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+import SearchForm from './Components/SearchForm';
+import GifList from './Components/GifList';
+
+export default class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      gifs: [],
+      loading: true
+    };
+  } 
+
+  componentDidMount() {
+    
+    // FETCH API CALLS:  
+
+    // fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+    // .then(response => response.json())
+    // .then(responseData => {
+    //   this.setState({ gifs: responseData.data });
+    // })
+    // .catch(error => {
+    //   console.log('Error fetching and parsing data', error);
+    // }
+    // );
+    
+    // AXIOS API Endpoint Method:  
+    // Automatically parses the response into JSON data
+  axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+  .then(response => {
+    this.setState({
+      gifs:  response.data.data
+    });
+  }) 
+    .catch(error => {
+      // handle error
+      console.log('Error Fetching and Parsing Data', error);
+    });
+
+  }
+
+  componentDidMount() {
+    this.performSearch();
+  }
+
+  performSearch = (query = 'dogs') => {
+       // AXIOS API Endpoint Method:  
+    // Automatically parses the response into JSON data
+  axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC 
+  `)
+  .then(response => {
+    this.setState({
+      gifs:  response.data.data,
+      loading: false
+    });
+  }) 
+    .catch(error => {
+      // handle error
+      console.log('Error Fetching and Parsing Data', error);
+    });
+  }
+
+  render() { 
+    console.log(this.state.gifs);
+    return (
+      <div>
+        <div className="main-header">
+          <div className="inner">
+            <h1 className="main-title">GifSearch</h1>
+            <SearchForm onSearch={this.performSearch} />      
+          </div>   
+        </div>    
+        <div className="main-content">
+          {
+            (this.state.loading)
+            ? <p>Loading....</p>
+            : <GifList data={this.state.gifs} />
+          }
+        </div>
+      </div>
+    );
+  }
+}
